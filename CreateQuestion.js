779be18752160730c4ae01f244e01b44dc84eee2
emandura <emandura98@gmail.com>
2020-06-21 17:02:03 +0200

    Update CreateQuestion.js

--- CreateQuestion.js
+++ CreateQuestion.js
@@ -10,26 +10,60 @@ class CreateQuestion extends React.Component {
             optionC: "",
             optionD: "",
             rightAnswer: "",
-            count:0
+            testID: "",
+            counter: 1,
+            counterID: 1
         }
-      this.handleChange = this.handleChange.bind(this)
-      this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+
 
     handleChange(event)  {
       const {name, value, type, checked, id} = event.target;
        if( type === "checkbox") {
           if(checked) {
-           this.setState({ [name]: value });
+           this.setState({ rightAnswer: value });
           }
         }
         this.setState({ [name]: value });
-      let questions=this.state;
-      localStorage.setItem('myData', JSON.stringify(questions));
-      let data=localStorage.getItem('myData');
-      data=JSON.parse(data);
-      console.log(data)
+      console.log(this.state);
+}
 
+      handleSubmit() {
+
+          fetch("http://localhost:3000/dev/tests")
+            .then(response => response.json())
+            .then(data => {
+                this.data.map((item)=> { 
+                    this.setState(prevState => ({
+                          counter: prevState.counter + 1
+                         }));
+                    if(this.state.counter%5==0){
+                      this.setState(prevState => ({
+                         counterID: prevState.counterID + 1
+                     }));
+                    }   
+                    })
+                 });
+
+    const requestOptions = {
+        method: 'POST',
+        body: JSON.stringify({ 
+            id: this.props.number,
+            question: this.state.question,
+            optionA: this.state.optionA,
+            optionB: this.state.optionB,
+            optionC: this.state.optionC,
+            optionD: this.state.optionD,
+            rightAnswer: this.state.rightAnswer,
+            testID: this.state.counterID
+        })
+    };
+    fetch('http://localhost:3000/dev/tests', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
 }
 
   
@@ -37,17 +71,17 @@ class CreateQuestion extends React.Component {
      render() {
         return (
         
-<div className="testCreate"><form>
+<div className="testCreate"><form onSubmit={this.handleSubmit}>
   <label for="questionCreate">Question {this.props.number}: </label>
   <input type="text" value={this.state.question} name="question" onChange={this.handleChange} placeholder="Enter question text"/>
   <label for="answerCreate">Answer options: </label>
   <div class="createTestInput">
   <input type="text" value={this.state.optionA} name="optionA" onChange={this.handleChange} placeholder="Enter option number 1"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" value={this.state.optionA} id="first"/>
-  <input type="text" value={this.state.optionB} name="optionB" onChange={this.handleChange} placeholder="Enter option number 2"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" id="second"/>
-  <input type="text" value={this.state.optionC} name="optionC" onChange={this.handleChange} placeholder="Enter option number 3"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" id="third"/>
-  <input type="text" value={this.state.optionD} name="optionD" onChange={this.handleChange} placeholder="Enter option number 4"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" id="fourth"/>
+  <input type="text" value={this.state.optionB} name="optionB" onChange={this.handleChange} placeholder="Enter option number 2"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" value={this.state.optionB} id="second"/>
+  <input type="text" value={this.state.optionC} name="optionC" onChange={this.handleChange} placeholder="Enter option number 3"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" value={this.state.optionC} id="third"/>
+  <input type="text" value={this.state.optionD} name="optionD" onChange={this.handleChange} placeholder="Enter option number 4"/><label for="checkbox">Right answer?</label><input type="checkbox" onChange={this.handleChange} name="rigthAnswer" value={this.state.optionD} id="fourth"/>
   </div>
-  <hr/>
+  <input type="submit"/>
 </form>
 </div>
 

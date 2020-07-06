aedde3abe9298386f8a143d079a5e17ced3759d2
emandura <emandura98@gmail.com>
2020-07-06 19:42:37 +0200

    Update Header.js

--- Header.js
+++ Header.js
@@ -11,11 +11,23 @@ import './index.css';
 function Header() {
     return (
         <header>
-            <p>Math online</p>
+       <p>Math online</p>
+            <div className="dropdown">
+  <Link to={{pathname: "/menu"}}><button className="dropbtn">Menu</button></Link>
+  <div className="dropdown-content">
+    <Link to={{pathname: "/chapters/1"}}>First Grade</Link>
+    <Link to={{pathname: "/chapters/2"}}>Second Grade</Link>
+  	<Link to={{pathname: "/chapters/3"}}>Third Grade</Link>
+    <Link to={{pathname: "/chapters/4"}}>Fourth Grade</Link>
+  </div>
+</div>
+
         </header>
 
     )
 }
 
 export default Header
+
+
 
